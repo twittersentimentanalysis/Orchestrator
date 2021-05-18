d9e7c967f7a44da9358945eef9ebba4b216fca90
Ariadna de Arriba <aridease@gmail.com>
2021-05-18 12:20:42 +0200

    Remove function accuracy ParallelDots

--- src/main/java/twitter/Data.java
+++ src/main/java/twitter/Data.java
@@ -57,103 +57,6 @@ public class Data
         }
     }
 
-    public static void getAccuracyParallelDots() throws IOException
-    {
-        System.out.println("START HERE");
-        SentimentAnalysisAPI staticSentimentAnalysisAPIs = new SentimentAnalysisAPI();
-        Properties staticProperties = ConfigProperties.readProperties();
-        try (CSVReader reader = new CSVReader(new FileReader("data/twitter/testing-emotion-dataset-nlp.csv")))
-        {
-
-            float diag = 220;
-            float all = 1620;
-            List<String[]> lines = reader.readAll();
-            String[] line;
-            int index = 969;
-            int[][] cf = {{134, 204, 43}, {71, 315, 66}, {65, 281, 118}};
-
-            //while ((line = reader.readNext()) != null)
-            while ((line = lines.get(index)) != null)
-            {
-                String text = line[3];
-                String emotion = line[4];
-                System.out.println("TEXT " + line[3]);
-                System.out.println("EMOTION " + line[4]);
-
-                // Get emotion from ParallelDots
-                String testingEmotionsString = staticSentimentAnalysisAPIs.getEmotion(text, staticProperties.getProperty("ml_tool"), staticProperties.getProperty("ml_tool_api-key"));
-
-                JsonObject json = JsonParser.parseString(testingEmotionsString).getAsJsonObject();
-                JsonObject emotionJson = json.getAsJsonObject("emotion");
-
-                System.out.println(emotionJson.toString());
-
-                float bored = parseFloat(emotionJson.get("Bored").toString());
-                float angry = parseFloat(emotionJson.get("Angry").toString());
-                float sad = parseFloat(emotionJson.get("Sad").toString());
-                float fear = parseFloat(emotionJson.get("Fear").toString());
-                float happy = parseFloat(emotionJson.get("Happy").toString());
-                float excited = parseFloat(emotionJson.get("Excited").toString());
-
-                Map emotions = new HashMap<Float, String>();
-                emotions.put(bored, "bored");
-                emotions.put(angry, "angry");
-                emotions.put(sad, "sad");
-                emotions.put(fear, "fear");
-                emotions.put(happy, "happy");
-                emotions.put(excited, "excited");
-
-                float max = Math.max(Math.max(Math.max(Math.max(Math.max(bored, angry), sad), fear), happy), excited);
-
-                String predicted = emotions.get(max).toString();
-                System.out.println("PREDICTED " + predicted);
-
-                if (emotion.equals(predicted))
-                {
-                    diag++;
-                    if (emotion.equals("happy")) cf[0][0]++;
-                    else if (emotion.equals("sad")) cf[1][1]++;
-                    else if (emotion.equals("angry")) cf[2][2]++;
-                }
-                else
-                {
-                    if (emotion.equals("happy"))
-                    {
-                        if (predicted.equals("sad")) cf [0][1]++;
-                        else if (predicted.equals("angry")) cf[0][2]++;
-                    }
-                    else if (emotion.equals("sad"))
-                    {
-                        if (predicted.equals("happy")) cf[1][0]++;
-                        else if (predicted.equals("angry")) cf[1][2]++;
-                    }
-                    else if (emotion.equals("angry"))
-                    {
-                        if (predicted.equals("happy")) cf[2][0]++;
-                        else if (predicted.equals("sad")) cf[2][1]++;
-                    }
-                }
-
-                System.out.println("MAX: " + max + " DIAG: " + diag);
-                index++;
-
-                int i = 0; int j = 0;
-                System.out.println("CONFUSION MATRIX: ");
-                for (i = 0; i < cf.length; i++) {
-                    for (j = 0; j < cf[i].length; j++) {
-                        System.out.print(cf[i][j] + " ");
-                    }
-                    System.out.println();
-                }
-
-                System.out.println("INDEX: " + index);
-            }
-            float accuracy = diag/all;
-            System.out.println("Accuracy: " + accuracy);
-        }
-    }
-
-
     /** Applies sentiment analysis into a tweet read it from Kafka server.
      *  The result at the end of execution is a csv file containing the text of all tweets and a json string for each one
      *  with six emotions weighted (sentiment analysis result).
@@ -208,9 +111,8 @@ public class Data
         System.out.println(jsonResponse);
         JsonObject jsonObj = JsonParser.parseString(jsonResponse.toString()).getAsJsonObject();
         String cleanText = jsonObj.get("text").toString().replace("\"", "");;
-        String polarity = jsonObj.get("polarity").getAsString();
 
-        String[] lineCSV = new String[]{text, cleanText, polarity};
+        String[] lineCSV = new String[]{text, cleanText};
         System.out.println(Arrays.toString(lineCSV));
 
         writerInput.writeNext(lineCSV, false);           // Writing data to the csv file
